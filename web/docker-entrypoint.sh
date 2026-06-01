3e166f72637a16d6140bf70bfff90e04490827df
Philip James <phildini@phildini.net>
2026-06-01 07:29:21 +0100

    Add release command for migrations, remove migrate from entrypoint, add deploy timeout

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -5,6 +5,4 @@ if [ "$1" = "python" ] && [ "$2" = "manage.py" ] && [ "$3" = "qcluster" ]; then
     exec python manage.py qcluster
 fi
 
-mkdir -p /data
-python manage.py migrate --noinput
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8080 --workers 1 --threads 4 --timeout 120 --access-logfile -
