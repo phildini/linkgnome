6b018fa9ed52ad079f7e0da227e330c97b26ecdf
Philip James <phildini@phildini.net>
2026-06-01 22:22:43 +0100

    Switch to litefs mount as ENTRYPOINT; single machine runs gunicorn + qcluster

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-mkdir -p /data /var/lib/litefs
-
-litefs mount -config /etc/litefs.yml &
-
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30; do
     if python -c "
 import os, sqlite3
@@ -17,10 +13,8 @@ if os.path.getsize('/data/linkgnome.db') > 0:
     sleep 1
 done
 
-if [ "$1" = "python" ] && [ "$2" = "manage.py" ] && [ "$3" = "qcluster" ]; then
-    exec python manage.py qcluster
-fi
-
 python manage.py migrate --noinput
+
+python manage.py qcluster &
 
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8080 --workers 1 --threads 4 --timeout 120 --access-logfile -
